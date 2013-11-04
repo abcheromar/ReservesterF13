034b196b56d378e3d29fa0e066fce6679f53f6f7
planbman <abshirguul@gmail.com>
2013-11-04 13:39:02 -0500

    initial commit for the 100th time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbshirAmberreReservesterf13::Application.config.secret_key_base = '6310b57e700106d3790ef79a5e07da9e642afad3517135e11042f69f151740fe406e05c1fd6ce19af824ea1e6f9ed44ee13ec38a1968ac947126adeeffb0772e'
